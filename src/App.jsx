79a016e26b3b75530b8c0f7dde026874637e0edd
Siddiq Prince <4mh19cs053@gmail.com>
2025-07-30 15:20:10 +0530

    app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
-import Projects from "./Pages/Projects"; // ✅ Import this
+import Projects from "./Pages/Projects";
 
 function App() {
   return (
@@ -10,8 +10,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
